06904408fe0d10e6b8c9ca86d3b6de6300008af6
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-12 23:38:15 +0700

    components & props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,22 @@
+import React from "react";
+
+const Button = (props) => {
+  const {children, variant = "bg-black"} = props;
+  return (
+    <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">
+      {props.children}    
+    </button>
+    
+  );
+}
+
 function App() {
   return (<div className='flex justify-center bg-blue-200 min-h-screen items-center'>
-    <div className="">Hello World</div>
+    <div className="flex gap-x-3">
+      <Button variant="bg-red-600">Login</Button>
+      <Button variant="bg-slate-600">Logout</Button>
+      <Button>Register</Button>
+    </div>
   </div>
   );
 }
