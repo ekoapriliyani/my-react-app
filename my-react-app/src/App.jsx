d5c1f0443e8a9ce58f1a60a46c29eff7d5ef587f
ekoapriliyani <72810997+ekoapriliyani@users.noreply.github.com>
2025-01-13 19:31:22 +0700

    atomic design

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 
-const Button = (props) => {
-  const {children, variant = "bg-black"} = props;
-  return (
-    <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">
-      {props.children}    
-    </button>
-    
-  );
-}
-
 function App() {
-  return (<div className='flex justify-center bg-blue-200 min-h-screen items-center'>
+  return (<div className='flex justify-center min-h-screen items-center'>
     <div className="flex gap-x-3">
-      <Button variant="bg-red-600">Login</Button>
-      <Button variant="bg-slate-600">Logout</Button>
-      <Button>Register</Button>
+      <div className="w-full max-w-xs">
+        <h1 className="text-3xl font-bold mb-2 text-blue-600">Login</h1>
+        <p className="font-medium text-slate-500 mb-8">
+          Welcome, Please enter your details
+        </p>
+        <form action="">
+          <div className="mb-6">
+            <label htmlFor="email" 
+              className="block text-slate-700 font-bold mb-2">
+              Email
+            </label>
+            <input 
+              type="text" 
+              className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50"
+              placeholder="example@email.com"
+             />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" 
+              className="block text-slate-700 font-bold mb-2">
+              Password
+            </label>
+            <input 
+              type="password" 
+              className="text-sm border rounded w-full py-2 px-3 text-slate-700 placeholder:opacity-50"
+              placeholder="***"
+             />
+          </div>
+        </form>
+      </div>
     </div>
   </div>
   );
